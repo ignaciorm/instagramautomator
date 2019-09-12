81d4802cbf4671ecd93485dad34daf5341c1b985
ignaciorm <jirm2062@gmail.com>
2019-09-12 11:26:25 +0100

    Timespace = 3 days

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ import time
 from os import listdir
 from os.path import isfile, join
 
+
 username = '_xcel_ior'
 password = 'Cooligna77'
 path = 'queue/'
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))] # onlyfiles is an array containing the filenames of all queued photos
-
 n = 0
+secondsinaday = 86400
+timespace = secondsinaday * 3
 
-
-text = 'This is just a test'
-
+text = '#streetphotography'
 
 def publish(image, caption):
-    # publishes image using photo path(image) and a caption, it also deletes the image from the queue folder
+    # publishes image using photo path(image) and a caption
     with client(username, password) as cli:
         cli.upload('queue/' + image, caption)
     print('Published')
 
 while __name__ == '__main__':
+    print(onlyfiles[n])
     publish(onlyfiles[n], text)
     n += 1
-    time.sleep(5)
+    time.sleep(timespace)
