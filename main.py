e7b5c08060fcc35b86d4f14887d761f5e9c80fd5
ignaciorm <jirm2062@gmail.com>
2019-09-12 10:01:49 +0100

    continuous upload working

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from instapy_cli import client
 import os
+import time
 from os import listdir
 from os.path import isfile, join
 
 username = '_xcel_ior'
 password = 'Cooligna77'
 path = 'queue/'
-onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-#onlyfiles is an array containing the filenames of all queued photos
+onlyfiles = [f for f in listdir(path) if isfile(join(path, f))] # onlyfiles is an array containing the filenames of all queued photos
 
-def publish(image, caption):
-    #publishes image using photo path(image) and a caption, it also deletes the image from the queue folder
-    with client(username, password) as cli:
-        cli.upload('queue/' + image, caption)
-    os.remove('queue/' + image)
+n = 0
+
 
 text = 'This is just a test'
 
-publish(onlyfiles[0], text)
+
+def publish(image, caption):
+    # publishes image using photo path(image) and a caption, it also deletes the image from the queue folder
+    with client(username, password) as cli:
+        cli.upload('queue/' + image, caption)
+    print('Published')
+
+while __name__ == '__main__':
+    publish(onlyfiles[n], text)
+    n += 1
+    time.sleep(5)
